706a9a7c50293532787a33340f857a4165e1992f
daeungkim <daeungkim93@gmail.com>
2020-04-21 21:08:04 +0900

    add Place data model

--- server/src/types/graph.d.ts
+++ server/src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type User {\n  id: Int!\n  email: String!\n  userName: String!\n  profilePhoto: String!\n  bio: String\n  password: String!\n  createAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n"];
+export const typeDefs = ["type Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String!\n  userName: String!\n  profilePhoto: String!\n  bio: String\n  password: String!\n  createAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -13,5 +13,16 @@ export interface User {
   bio: string | null;
   password: string;
   createAt: string;
+  updatedAt: string | null;
+}
+
+export interface Place {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  isFav: boolean;
+  createdAt: string;
   updatedAt: string | null;
 }
