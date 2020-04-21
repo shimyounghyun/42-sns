5bfe0183ddb9f652aea443a5914de24db1ea4b48
daeungkim <daeungkim93@gmail.com>
2020-04-21 21:32:09 +0900

    add Trip data model

--- server/src/types/graph.d.ts
+++ server/src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String!\n  userName: String!\n  profilePhoto: String!\n  bio: String\n  password: String!\n  createAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n"];
+export const typeDefs = ["type Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Trip {\n  id: Int!\n  status: String!\n  destinationAddress: String!\n  destinationLat: Float!\n  destinationLng: Float!\n  startDay: String!\n  endDay: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String!\n  userName: String!\n  profilePhoto: String!\n  bio: String\n  password: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  user: User\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -12,7 +12,7 @@ export interface User {
   profilePhoto: string;
   bio: string | null;
   password: string;
-  createAt: string;
+  createdAt: string;
   updatedAt: string | null;
 }
 
@@ -23,6 +23,18 @@ export interface Place {
   lng: number;
   address: string;
   isFav: boolean;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Trip {
+  id: number;
+  status: string;
+  destinationAddress: string;
+  destinationLat: number;
+  destinationLng: number;
+  startDay: string;
+  endDay: string;
   createdAt: string;
   updatedAt: string | null;
 }
