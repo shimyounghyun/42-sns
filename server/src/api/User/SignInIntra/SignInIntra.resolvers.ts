fde0bc3bafcf78cd4b1d9d6d1b30c486a2f051e4
daeungkim <daeungkim93@gmail.com>
2020-04-24 01:36:54 +0900

    add Sign in intra function

---
+++ server/src/api/User/SignInIntra/SignInIntra.resolvers.ts
@@ -0,0 +1,26 @@
+import { SignInIntraQueryArgs, SignInIntraResponse } from "src/types/graph";
+import { Resolvers } from "src/types/resolvers";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Query: {
+    SignInIntra: async (
+      _,
+      args: SignInIntraQueryArgs
+    ): Promise<SignInIntraResponse> => {
+      try {
+        const existingUser = await User.findOne(args);
+
+        if (existingUser) {
+          return { result: true, error: null, token: "Log In" };
+        } else {
+          return { result: false, error: null, token: null };
+        }
+      } catch (error) {
+        return { result: false, error: error.message, token: null };
+      }
+    },
+  },
+};
+
+export default resolvers;
