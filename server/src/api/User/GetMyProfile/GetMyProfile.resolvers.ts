90bca1fbb753c7a220ff9bdaf1ef59358e549aab
daeungkim <daeungkim93@gmail.com>
2020-04-25 01:44:17 +0900

    protecting resolvers  with middlewares

--- server/src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
+++ server/src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
@@ -1,16 +1,19 @@
 import { GetMyProfileResponse } from "src/types/graph";
 import { Resolvers } from "src/types/resolvers";
+import privateResolver from "../../../utils/privateMiddleware";
 
 const resolvers: Resolvers = {
   Query: {
-    GetMyProfile: (_, __, { req }): GetMyProfileResponse => {
-      const { user } = req;
-      return {
-        ok: true,
-        error: null,
-        user,
-      };
-    },
+    GetMyProfile: privateResolver(
+      async (_, __, { req }): Promise<GetMyProfileResponse> => {
+        const { user } = req;
+        return {
+          ok: true,
+          error: null,
+          user,
+        };
+      }
+    ),
   },
 };
 
---
+++ server/src/utils/privateMiddleware.ts
@@ -0,0 +1,14 @@
+const privateResolver = (resovlerFunction) => async (
+  parent,
+  args,
+  context,
+  info
+) => {
+  if (!context.req.user) {
+    throw new Error("No JWT. I refuse to proceed");
+  }
+  const resolved = await resovlerFunction(parent, args, context, info);
+  return resolved;
+};
+
+export default privateResolver;
