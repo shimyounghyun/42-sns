d0ddeeaed513d159113fa46df3d4595f33c0d502
daeungkim <daeungkim93@gmail.com>
2020-04-27 18:08:05 +0900

    add types

--- server/src/types/graph.d.ts
+++ server/src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]!\n  participants: [User]!\n  trip: Trip\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: Chat!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Date {\n  id: Int!\n  time: String!\n  trips: [Trip]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  trips: [Trip]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Trip {\n  id: Int!\n  status: String!\n  host: User!\n  guest: User\n  dates: [Date]\n  chat: Chat\n  place: Place\n  createdAt: String!\n  updatedAt: String\n}\n\ntype EmailSignInResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype Query {\n  EmailSignIn(email: String!, password: String!): EmailSignInResponse\n  GetMyProfile: GetMyProfileResponse\n  SignInIntra(intraId: String!): SignInIntraResponse\n}\n\ntype GetMyProfileResponse {\n  result: Boolean!\n  error: String\n  user: User\n}\n\ntype IntraConnectResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  IntraConnect(email: String!, userName: String!, firstName: String!, lastName: String!, profilePhoto: String!, intraId: String!): IntraConnectResponse\n  SignUpIntra(email: String!, userName: String!, firstName: String!, lastName: String!, profilePhoto: String!, intraId: String!, password: String!): SignUpIntraResponse\n  UpdateMyProfile(bio: String, profilePhoto: String): UpdateMyProfileResponse\n  UpdatePassword(previousPassword: String!, presentPassword: String!): UpdatePasswordResponse\n}\n\ntype User {\n  id: Int!\n  email: String!\n  userName: String!\n  firstName: String!\n  lastName: String!\n  fullName: String\n  profilePhoto: String!\n  bio: String\n  password: String!\n  chats: [Chat]\n  messages: [Message]\n  tripAsHost: [Trip]\n  tripAsGuest: [Trip]\n  intraId: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInIntraResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype SignUpIntraResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype UpdateMyProfileResponse {\n  result: Boolean!\n  error: String\n}\n\ntype UpdatePasswordResponse {\n  result: Boolean!\n  error: String\n}\n"];
+export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]!\n  participants: [User]!\n  trip: Trip\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: Chat!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Date {\n  id: Int!\n  time: String!\n  trips: [Trip]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype AddPlaceResponse {\n  result: Boolean!\n  error: String\n}\n\ntype Mutation {\n  AddPlace(name: String!, lat: Float!, lng: Float!, address: String!): AddPlaceResponse!\n  EditPlace(placeId: Int!, name: String): EditPlaceResponse!\n  IntraConnect(email: String!, userName: String!, firstName: String!, lastName: String!, profilePhoto: String!, intraId: String!): IntraConnectResponse\n  SignUpIntra(email: String!, userName: String!, firstName: String!, lastName: String!, profilePhoto: String!, intraId: String!, password: String!): SignUpIntraResponse\n  UpdateMyProfile(bio: String, profilePhoto: String): UpdateMyProfileResponse\n  UpdatePassword(previousPassword: String!, presentPassword: String!): UpdatePasswordResponse\n}\n\ntype EditPlaceResponse {\n  result: Boolean!\n  error: String\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  trips: [Trip]\n  user: User!\n  userId: Int!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype AddTripResponse {\n  result: Boolean!\n  error: String\n}\n\ntype Trip {\n  id: Int!\n  status: String!\n  host: User!\n  guest: User\n  dates: [Date]\n  chat: Chat\n  place: Place\n  createdAt: String!\n  updatedAt: String\n}\n\ntype EmailSignInResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype Query {\n  EmailSignIn(email: String!, password: String!): EmailSignInResponse\n  GetMyProfile: GetMyProfileResponse\n  SignInIntra(intraId: String!): SignInIntraResponse\n}\n\ntype GetMyProfileResponse {\n  result: Boolean!\n  error: String\n  user: User\n}\n\ntype IntraConnectResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype User {\n  id: Int!\n  email: String!\n  userName: String!\n  firstName: String!\n  lastName: String!\n  fullName: String\n  profilePhoto: String!\n  bio: String\n  password: String!\n  places: [Place]\n  chats: [Chat]\n  messages: [Message]\n  tripAsHost: [Trip]\n  tripAsGuest: [Trip]\n  intraId: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInIntraResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype SignUpIntraResponse {\n  result: Boolean!\n  error: String\n  token: String\n}\n\ntype UpdateMyProfileResponse {\n  result: Boolean!\n  error: String\n}\n\ntype UpdatePasswordResponse {\n  result: Boolean!\n  error: String\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -38,6 +38,7 @@ export interface User {
   profilePhoto: string;
   bio: string | null;
   password: string;
+  places: Array<Place> | null;
   chats: Array<Chat> | null;
   messages: Array<Message> | null;
   tripAsHost: Array<Trip> | null;
@@ -47,20 +48,15 @@ export interface User {
   updatedAt: string | null;
 }
 
-export interface Chat {
+export interface Place {
   id: number;
-  messages: Array<Message>;
-  participants: Array<User>;
-  trip: Trip | null;
-  createdAt: string;
-  updatedAt: string | null;
-}
-
-export interface Message {
-  id: number;
-  text: string;
-  chat: Chat;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  trips: Array<Trip> | null;
   user: User;
+  userId: number;
   createdAt: string;
   updatedAt: string | null;
 }
@@ -85,13 +81,20 @@ export interface Date {
   updatedAt: string | null;
 }
 
-export interface Place {
+export interface Chat {
   id: number;
-  name: string;
-  lat: number;
-  lng: number;
-  address: string;
-  trips: Array<Trip> | null;
+  messages: Array<Message>;
+  participants: Array<User>;
+  trip: Trip | null;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Message {
+  id: number;
+  text: string;
+  chat: Chat;
+  user: User;
   createdAt: string;
   updatedAt: string | null;
 }
@@ -103,10 +106,24 @@ export interface SignInIntraResponse {
 }
 
 export interface Mutation {
+  AddPlace: AddPlaceResponse;
+  EditPlace: EditPlaceResponse;
   IntraConnect: IntraConnectResponse | null;
   SignUpIntra: SignUpIntraResponse | null;
   UpdateMyProfile: UpdateMyProfileResponse | null;
   UpdatePassword: UpdatePasswordResponse | null;
+}
+
+export interface AddPlaceMutationArgs {
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+}
+
+export interface EditPlaceMutationArgs {
+  placeId: number;
+  name: string | null;
 }
 
 export interface IntraConnectMutationArgs {
@@ -138,6 +155,16 @@ export interface UpdatePasswordMutationArgs {
   presentPassword: string;
 }
 
+export interface AddPlaceResponse {
+  result: boolean;
+  error: string | null;
+}
+
+export interface EditPlaceResponse {
+  result: boolean;
+  error: string | null;
+}
+
 export interface IntraConnectResponse {
   result: boolean;
   error: string | null;
@@ -156,6 +183,11 @@ export interface UpdateMyProfileResponse {
 }
 
 export interface UpdatePasswordResponse {
+  result: boolean;
+  error: string | null;
+}
+
+export interface AddTripResponse {
   result: boolean;
   error: string | null;
 }
