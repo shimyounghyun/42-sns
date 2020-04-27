4682074369e6fb67df81b3a17719de6ee225fbd0
daeungkim <daeungkim93@gmail.com>
2020-04-27 18:01:34 +0900

    fix update my profile function

--- server/src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ server/src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -4,6 +4,7 @@ import {
 } from "src/types/graph";
 import { Resolvers } from "src/types/resolvers";
 import User from "../../../entities/User";
+import cleanNullArgs from "../../../utils/cleanNullArgs";
 import privateResolver from "../../../utils/privateMiddleware";
 
 const resolvers: Resolvers = {
@@ -16,19 +17,8 @@ const resolvers: Resolvers = {
       ): Promise<UpdateMyProfileResponse> => {
         const user: User = req.user;
 
-        // 비밀번호만 바꾸고 싶은경우
-        // 다른 값들은 null이기 때문에
-        // 그대로 데이터를 업로드하면 문제가 생길 수 있음
-        // 따라서 null인 값들을 없애주는 작업이 필요함
-        const notNull = {};
-        Object.keys(args).forEach((key) => {
-          // Object.key() = 객체의 키를 리턴함
-          if (args[key] !== null) {
-            // args의 각각의 키를 통해 값을 확인함
-            // 값이 null인경우를 제외하고 notNull에 값을 넣어줌
-            notNull[key] = args[key];
-          }
-        });
+        // 입력하지 않아 null값 들어가있는 경우 처리
+        const notNull = cleanNullArgs(args);
 
         // 데이터 업로드 함
         try {
