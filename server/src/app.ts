7e02e8f124281ffbd6c2f07f7bcf4ddf37d052d9
daeungkim <daeungkim93@gmail.com>
2020-05-02 18:14:27 +0900

    add subscription settings

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,34 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import { NextFunction, Response } from "express";
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
 import decodeJWT from "./utils/decodeJWT";
 
+dotenv.config();
+
 class App {
   public app: GraphQLServer;
+
+  // subscription을 위해 사용함
+  public pubSub: any;
 
   // schema : 모든 graphql 파일과 그에 해당하는 resolver 파일이 들어있음
   // context : express의 req값이 들어있음 (ex: 로그인한 유저 정보 등)
   constructor() {
+    // pubsub의 경우 데모에만 사용할 것
+    this.pubSub = new PubSub();
+    this.pubSub.ee.setMaxListeners(99);
+
+    // context에 필요한 데이터를 넣음
     this.app = new GraphQLServer({
       schema,
       context: (req) => {
         return {
           req: req.request,
+          pubSub: this.pubSub,
         };
       },
     });
