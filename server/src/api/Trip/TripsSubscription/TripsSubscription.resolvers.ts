5eead6c8c1eb4c043da88d37662bd6a998ec2434
daeungkim <daeungkim93@gmail.com>
2020-05-02 20:24:40 +0900

    add subscription auth

--- server/src/api/Trip/TripsSubscription/TripsSubscription.resolvers.ts
+++ server/src/api/Trip/TripsSubscription/TripsSubscription.resolvers.ts
@@ -1,9 +1,19 @@
+import { withFilter } from "graphql-yoga";
+import User from "../../../entities/User";
+
 const resolvers = {
   Subscription: {
     TripsSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("tripUpdate");
-      },
+      subscribe: withFilter(
+        (_, __, { pubSub }) => {
+          return pubSub.asyncIterator("tripUpdate");
+        },
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          if (payload.guestId === user.id) return true;
+          return false;
+        }
+      ),
     },
   },
 };
