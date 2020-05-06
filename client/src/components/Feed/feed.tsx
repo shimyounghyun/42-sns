47e08b6d2dda21e88fd89c2b06d6ab329e496daa
donglee <donglee@student.42seoul.kr>
2020-05-06 16:59:42 +0900

    what the?

---
+++ client/src/components/Feed/feed.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import {gql} from 'apollo-boost';
+
+
+const FEED_QUERY = gql`
+    
+`
+/* all trips{
+    files,
+    captions, 
+    user(username, userphoto),
+    place,
+    dates,
+    createdAt
+},  */
